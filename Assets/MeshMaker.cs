1f6a76adf57947cc9f4bb74f7b1424e6d35b3783
Chris Stevens <munkeyxis@gmail.com>
2018-11-15 19:46:41 -0500

    dynamic mesh

---
+++ Assets/MeshMaker.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+ 
+public class MeshMaker : MonoBehaviour {
+    void Start () {
+        // Create Vector2 vertices
+        Vector2[] vertices2D = GetComponent<ClickableRegion>().Site.Region.ToArray();
+ 
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+ 
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i=0; i<vertices.Length; i++) {
+            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
+        }
+ 
+        // Create the mesh
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+        msh.RecalculateNormals();
+        msh.RecalculateBounds();
+ 
+        // Set up game object with mesh;
+        gameObject.AddComponent(typeof(MeshRenderer));
+        MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        filter.mesh = msh;
+    }
+}
